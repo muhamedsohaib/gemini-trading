a7f90af2cdce52e9fa5dde2c9d80c718930ca2de
Sohaib <muhammadsohaibsaif@gmail.com>
2026-07-23 14:09:16 +0400

    test: add research error taxonomy coverage

---
+++ tests/unit/research/test_errors.py
@@ -0,0 +1,10 @@
+from gemini_trading.research.errors import (
+    AccountingInvariantError,
+    DatasetVerificationError,
+    ResearchError,
+)
+
+
+def test_research_errors_share_one_safe_base() -> None:
+    assert issubclass(AccountingInvariantError, ResearchError)
+    assert issubclass(DatasetVerificationError, ResearchError)

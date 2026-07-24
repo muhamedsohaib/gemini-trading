38078849b9ee26e2477eeaed7de429ffc0863779
Sohaib <muhammadsohaibsaif@gmail.com>
2026-07-24 04:43:05 +0400

    fix: import property fixture helper consistently

--- tests/property/strategy/test_strategy_identity.py
+++ tests/property/strategy/test_strategy_identity.py
@@ -4,7 +4,7 @@ from dataclasses import replace
 
 from hypothesis import given
 from hypothesis import strategies as st
-from tests.strategy_fixture_support import example_study_manifest
+from strategy_fixture_support import example_study_manifest
 
 from gemini_trading.strategy.identity import study_id
 
